76c03ac530bf97743bb88201adca8741ed8fcf1d
Imran Khan <99397606+imran120198@users.noreply.github.com>
2023-08-07 09:37:35 +0530

    added user schema

--- Middleware/Authentication.js
+++ Middleware/Authentication.js
@@ -1,22 +1,21 @@
-const jwt = require("jsonwebtoken")
-require("dotenv").config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const authentication = (req,res,next) => {
-    if(!req.headers.authorization){
-        return res.status(401).send("Please Login Again")
+const authentication = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send("Please Login Again");
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+    if (err) {
+      res.status(401).send("Please Login");
+    } else {
+      req.body.userId = decoded.userId;
+      next();
     }
-    const token = req.headers.authorization.split(" ")[1]
-    jwt.verify(token, process.env.JWT_KEY, (err,decoded) => {
-        if(err){
-            res.status(401).send("Please Login")
-        }
-        else{
-            req.body.userId = decoded.userId;
-            next()
-        }
-    })
-}
+  });
+};
 
 module.exports = {
-    authentication
-}
+  authentication,
+};
