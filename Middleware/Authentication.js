cea6993cc75ef55c07b3a301553778fdf676a749
Imran Khan <99397606+imran120198@users.noreply.github.com>
2023-08-07 10:16:03 +0530

    authentication

--- Middleware/Authentication.js
+++ Middleware/Authentication.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 
 const authentication = (req, res, next) => {
   if (!req.headers.authorization) {
-    return res.status(401).send("Please Login Again");
+    return res.status(401).send("Please Login Again !!!");
   }
   const token = req.headers.authorization.split(" ")[1];
   jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
     if (err) {
-      res.status(401).send("Please Login");
+      res.status(401).send("Please Login Again !!!");
     } else {
       req.body.userId = decoded.userId;
       next();
