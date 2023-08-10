f027e6897521046f7057e09ff2c410e1909eb5eb
Imran Khan <99397606+imran120198@users.noreply.github.com>
2023-08-10 11:02:44 +0530

    login route

--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -8,27 +8,45 @@ const UserRouter = Router();
 
 UserRouter.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
-  const result = await UserModel.findOne({email})
-  if(result){
-    res.status(201).send({message : "Email Already Exist"})
-  }
-  else{
+  const result = await UserModel.findOne({ email });
+  if (result) {
+    res.status(201).send({ message: "Email Already Exist" });
+  } else {
     bcrypt.hash(password, 5, async (err, hash) => {
-      if(err){
-        res.status(500).send({message:"Something wrong with signup",err})
-      }
-      else{
+      if (err) {
+        res.status(500).send({ message: "Something wrong with signup", err });
+      } else {
         const newSignup = new UserModel({
-          username : username,
-          email : email,
-          password : hash
-        })
-        const saveSignup = newSignup.save()
-        res.status(201).send({message : "Signup Successfully", saveSignup})
+          username: username,
+          email: email,
+          password: hash,
+        });
+        const saveSignup = newSignup.save();
+        res.status(201).send({ message: "Signup Successfully", saveSignup });
       }
-    })
+    });
   }
-  
+});
+
+UserRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email });
+  const hash = user.hash;
+
+  bcrypt.compare(password, hash, async (err, result) => {
+    if (err) {
+      res.status(500).send({ message: "Something wrong with login" });
+    }
+    if (result) {
+      const expiresIn = "1d";
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+        expiresIn,
+      });
+      res.status(201).send({ message: "Login Successful", token });
+    } else {
+      res.status(500).send({ message: "Invalid Credential" });
+    }
+  });
 });
 
 module.exports = {
