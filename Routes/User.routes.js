1ddd204dc02bb2e4f46bfc9cbf83823d8a5bb0ba
Imran Khan <99397606+imran120198@users.noreply.github.com>
2023-08-10 10:39:55 +0530

    signup route

--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -9,6 +9,25 @@ const UserRouter = Router();
 UserRouter.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
   const result = await UserModel.findOne({email})
+  if(result){
+    res.status(201).send({message : "Email Already Exist"})
+  }
+  else{
+    bcrypt.hash(password, 5, async (err, hash) => {
+      if(err){
+        res.status(500).send({message:"Something wrong with signup",err})
+      }
+      else{
+        const newSignup = new UserModel({
+          username : username,
+          email : email,
+          password : hash
+        })
+        const saveSignup = newSignup.save()
+        res.status(201).send({message : "Signup Successfully", saveSignup})
+      }
+    })
+  }
   
 });
 
